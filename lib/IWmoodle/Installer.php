e1a967dfb5f4debefcbb101d9b885e691c8dcf4c
albert <aperez16@gmail.com>
2012-11-29 13:01:38 +0100

    Adapted to Agora

--- lib/IWmoodle/Installer.php
+++ lib/IWmoodle/Installer.php
@@ -1,86 +1,76 @@
 <?php
 
-/**
- * PostNuke Application Framework
- *
- * @copyright (c) 2002, PostNuke Development Team
- * @link http://www.postnuke.com
- * @version $Id: pninit.php 22139 2007-06-01 10:57:16Z markwest $
- * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
- * @package PostNuke_Value_Addons
- * @subpackage Webbox
- */
+class IWmoodle_Installer extends Zikula_AbstractInstaller {
 
-/**
- * initialise the IWmoodle module creating module tables and module vars
- * @author Albert Pérez Monfort (aperezm@xtec.cat)
- * @return bool true if successful, false otherwise
- */
-function IWmoodle_init() {
-    $dom = ZLanguage::getModuleDomain('IWmoodle');
-    // Checks if module iw_main is installed. If not returns error
-    $modid = ModUtil::getIdFromName('iw_main');
-    $modinfo = ModUtil::getInfo($modid);
+    /**
+     * initialise the IWmoodle module creating module tables and module vars
+     * @author Albert Pérez Monfort (aperezm@xtec.cat)
+     * @return bool true if successful, false otherwise
+     */
+    public function install() {
 
-    if ($modinfo['state'] != 3) {
-        return LogUtil::registerError(__('Module iw_main is needed. You have to install the iw_main module before to install it.', $dom));
+        // Checks if module IWmain is installed. If not returns error
+        $modid = ModUtil::getIdFromName('IWmain');
+        $modinfo = ModUtil::getInfo($modid);
+
+        if ($modinfo['state'] != 3) {
+            return LogUtil::registerError($this->__('Module IWmain is needed. You have to install the IWmain module before to install it.'));
+        }
+
+        // Check if the version needed is correct
+        $versionNeeded = '2.0';
+        if (!ModUtil::func('IWmain', 'admin', 'checkVersion', array('version' => $versionNeeded))) {
+            return false;
+        }
+
+
+        // Create module table
+        if (!DBUtil::createTable('IWmoodle'))
+            return false;
+
+        //Create module vars
+        $this->setVar('IWmoodle', 'moodleurl', '../moodle2')
+                ->setVar('IWmoodle', 'newwindow', 1)
+                ->setVar('IWmoodle', 'guestuser', 'guest')
+                ->setVar('IWmoodle', 'dbprefix', 'm2')
+                ->setVar('IWmoodle', 'dfl_description', 'User description')
+                ->setVar('IWmoodle', 'dfl_language', 'en_utf8')
+                ->setVar('IWmoodle', 'dfl_country', 'ES')
+                ->setVar('IWmoodle', 'dfl_city', 'City name')
+                ->setVar('IWmoodle', 'dfl_gtm', '99');
+        return true;
     }
 
-    // Check if the version needed is correct
-    $versionNeeded = '2.0';
-    if (!ModUtil::func('iw_main', 'admin', 'checkVersion', array('version' => $versionNeeded))) {
-        return false;
+    /**
+     * Delete the IWmoodle module
+     * @author Albert Pérez Monfort (aperezm@xtec.cat)
+     * @return bool true if successful, false otherwise
+     */
+    public function uninstall() {
+
+        // Delete module table
+        DBUtil::dropTable('IWmoodle');
+
+        //Delete module vars
+        $this->delVar('IWmoodle', 'moodleurl')
+                ->delVar('IWmoodle', 'newwindow')
+                ->delVar('IWmoodle', 'guestuser')
+                ->delVar('IWmoodle', 'dbprefix')
+                ->delVar('IWmoodle', 'dfl_description')
+                ->delVar('IWmoodle', 'dfl_language')
+                ->delVar('IWmoodle', 'dfl_country')
+                ->delVar('IWmoodle', 'dfl_city')
+                ->delVar('IWmoodle', 'dfl_gtm');
+        return true;
     }
 
+    /**
+     * Update the IWmoodle module
+     * @author Albert Pérez Monfort (aperezm@xtec.cat)
+     * @return bool true if successful, false otherwise
+     */
+    public function upgrade($oldversion) {
+        return true;
+    }
 
-    // Create module table
-    if (!DBUtil::createTable('IWmoodle'))
-        return false;
-
-    //Create module vars
-    ModUtil::setVar('IWmoodle', 'moodleurl', '../moodle2');
-    ModUtil::setVar('IWmoodle', 'newwindow', 1);
-    ModUtil::setVar('IWmoodle', 'guestuser', 'guest');
-    ModUtil::setVar('IWmoodle', 'dbprefix', 'm2');
-
-    ModUtil::setVar('IWmoodle', 'dfl_description', 'User description');
-    ModUtil::setVar('IWmoodle', 'dfl_language', 'en_utf8');
-    ModUtil::setVar('IWmoodle', 'dfl_country', 'ES');
-    ModUtil::setVar('IWmoodle', 'dfl_city', 'City name');
-    ModUtil::setVar('IWmoodle', 'dfl_gtm', '99');
-    return true;
 }
-
-/**
- * Delete the IWmoodle module
- * @author Albert Pérez Monfort (aperezm@xtec.cat)
- * @return bool true if successful, false otherwise
- */
-function IWmoodle_delete() {
-
-    // Delete module table
-    DBUtil::dropTable('IWmoodle');
-
-    //Delete module vars
-    ModUtil::delVar('IWmoodle', 'moodleurl');
-    ModUtil::delVar('IWmoodle', 'newwindow');
-    ModUtil::delVar('IWmoodle', 'guestuser');
-    ModUtil::delVar('IWmoodle', 'dbprefix');
-    ModUtil::delVar('IWmoodle', 'dfl_description');
-    ModUtil::delVar('IWmoodle', 'dfl_language');
-    ModUtil::delVar('IWmoodle', 'dfl_country');
-    ModUtil::delVar('IWmoodle', 'dfl_city');
-    ModUtil::delVar('IWmoodle', 'dfl_gtm');
-    return true;
-}
-
-/**
- * Update the IWmoodle module
- * @author Albert Pérez Monfort (aperezm@xtec.cat)
- * @return bool true if successful, false otherwise
- */
-function IWmoodle_upgrade($oldversion) {
-    return true;
-}
-
-?>
