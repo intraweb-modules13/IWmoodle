4201807be3806567db2225c7cd4859573d106864
albert <aperez16@gmail.com>
2013-05-22 10:40:57 +0200

    Fixed table definition problem

--- tables.php
+++ tables.php
@@ -18,27 +18,27 @@
  */
 function IWmoodle_tables() {
     // Initialise table array
-    $pntable = array();
+    $table = array();
 
     // iw_webbox table definition
-    $pntable['IWmoodle'] = DBUtil::getLimitedTablename('IWmoodle');
-    $pntable['IWmoodle_column'] = array('mid' => 'iw_mid',
+    $table['IWmoodle'] = DBUtil::getLimitedTablename('IWmoodle');
+    $table['IWmoodle_column'] = array('mid' => 'iw_mid',
         'mcid' => 'iw_mcid',
         'uid' => 'iw_uid',
         'role' => 'iw_role');
 
-    $pntable['IWmoodle_column_def'] = array('mid' => "INT(11) NOTNULL AUTOINCREMENT KEY",
-        'mcid' => "INT(11) NOTNULL DEFAULT '0'",
-        'uid' => "INT(11) NOTNULL DEFAULT '0'",
-        'role' => "INT(11) NOTNULL DEFAULT '0'");
+    $table['IWmoodle_column_def'] = array('mid' => "I NOTNULL AUTO PRIMARY",
+        'mcid' => "I NOTNULL DEFAULT '0'",
+        'uid' => "I NOTNULL DEFAULT '0'",
+        'role' => "I NOTNULL DEFAULT '0'");
 
     /*
       // Define tables from groups and users that are used in this module
-      $pntable['group_membership'] = DBUtil::getLimitedTablename('group_membership');
-      $pntable['group_membership_column'] = array('pn_uid' => 'pn_uid');
+      $table['group_membership'] = DBUtil::getLimitedTablename('group_membership');
+      $table['group_membership_column'] = array('pn_uid' => 'pn_uid');
 
-      $pntable['users'] = DBUtil::getLimitedTablename('users');
-      $pntable['users_column'] = array('pn_uid' => 'pn_uid',
+      $table['users'] = DBUtil::getLimitedTablename('users');
+      $table['users_column'] = array('pn_uid' => 'pn_uid',
       'pn_pass' => 'pn_pass',
       'pn_uname' => 'pn_uname');
      */
@@ -46,9 +46,9 @@ function IWmoodle_tables() {
 
 
 
-    ObjectUtil::addStandardFieldsToTableDefinition($pntable['IWmoodle_column'], 'pn_');
-    ObjectUtil::addStandardFieldsToTableDataDefinition($pntable['IWmoodle_column_def'], 'iw_');
+    ObjectUtil::addStandardFieldsToTableDefinition($table['IWmoodle_column'], 'pn_');
+    ObjectUtil::addStandardFieldsToTableDataDefinition($table['IWmoodle_column_def'], 'iw_');
 
     // Return the table information
-    return $pntable;
+    return $table;
 }
